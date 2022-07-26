e1f5c70ca609e5755eb6356da6a3f1c559ca2f1e
Gabriel Alcantara <gabrielalcantara39@gmail.com>
2022-07-26 13:42:18 -0300

    Grid de exibição dos commics

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,19 +5,41 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* comment link
         <img src={logo} className="App-logo" alt="logo" />
+ 
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+       <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+      <div className="grid-container">
+        <div className="grid-item">
+          <img src={logo} className="Comics-template" alt="comics" />
+        </div>
+        <div className="grid-item">
+          <img src={logo} className="Comics-template" alt="comics" />
+        </div>
+        <div className="grid-item">
+          <img src={logo} className="Comics-template" alt="comics" />
+        </div>
+        <div className="grid-item">
+          <img src={logo} className="Comics-template" alt="comics" />
+        </div>
+        <div className="grid-item">
+          <img src={logo} className="Comics-template" alt="comics" />
+        </div>
+        <div className="grid-item">
+          <img src={logo} className="Comics-template" alt="comics" />
+        </div>
+      </div>
     </div>
   );
 }
