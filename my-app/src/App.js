5741911a746fc8e98858e0f1c94a21a4b14dc946
Gabriel Alcantara <gabrielalcantara39@gmail.com>
2022-07-27 16:32:00 -0300

    Connected with Marvel API

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,41 @@
+import React, { Component } from 'react';
+import md5 from 'md5';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const publicurl ='https://gateway.marvel.com:443/v1/public/characters?';
+const publickey = '53049753204b6dc4157a9e4e02921ef6';
+const  privatekey = 'aa67cee82eccabf9abd26b56abe7128a94246da2';
+
+const time = Number(new Date());
+const hash = md5(time+privatekey+publickey);
+
+
+
+class App extends Component {   
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      totalReactPackages: null
+    };
+
+   }
+
+   async componentDidMount() {
+    const headers = {'Content-Type': 'application/json'}
+    const response = await fetch('https://gateway.marvel.com:443/v1/public/characters?ts='+time+'&apikey='+publickey+'&hash='+hash, {headers});
+    const data = await response.json();
+      this.setState({totalReactPackages: data});
+  }
+  
+  render() {
+    // See all react packages
+    const { totalReactPackages } = this.state;
+    return (
+      
     <div className="App">
       <header className="App-header">
         {/* comment link
@@ -25,20 +58,30 @@ function App() {
       <div className="grid-container">
         <div className="grid-item">
           <div className="card">
-            <img src={logo}></img>
-            <div className="card-info" alt="Avatar">
+              <img src={logo} className="Comics-template" alt="comics" />            
+              <div className="card-info" alt="Avatar">
               <h4><b>Comics</b></h4>
               <p>Descrition</p>
             </div>
           </div>
         </div>
         <div className="grid-item">
-          <img src={logo} className="Comics-template" alt="comics" />
+        <div className="card">
+              <img src={logo} className="Comics-template" alt="comics" />            
+              <div className="card-info" alt="Avatar">
+              <h4><b>Comics</b></h4>
+              
+
+            </div>
+          </div>
         </div>
         
       </div>
+
     </div>
+    
   );
+    }
 }
 
 export default App;
